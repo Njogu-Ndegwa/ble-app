843ac36942a1d21381672904207d0e91011ce0c5
Dennis <dennisndegwa001@gmail.com>
2025-04-15 09:11:16 +0300

    Update Code

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,35 @@
 // export default nextConfig;
 
 
-import withSerwistInit from "@serwist/next";
+// import withSerwistInit from "@serwist/next";
  
-const withSerwist = withSerwistInit({
-  swSrc: "src/app/sw.js",
-  swDest: "public/sw.js",
-});
+// const withSerwist = withSerwistInit({
+//   swSrc: "src/app/sw.js",
+//   swDest: "public/sw.js",
+// });
  
-export default withSerwist({});
+// export default withSerwist({});
+
+
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase:any) => {
+  /** @type {import("next").NextConfig} */
+  const nextConfig = {};
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      // Note: This is only an example. If you use Pages Router,
+      // use something else that works, such as "service-worker/index.ts".
+      swSrc: "app/sw.ts",
+      swDest: "public/sw.js",
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
+};
