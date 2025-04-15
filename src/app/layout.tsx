cbee8bf5c28b3b471face72fd346c6320bb941f6
Dennis <dennisndegwa001@gmail.com>
2025-04-15 09:18:17 +0300

    Update Code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 // import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
@@ -81,11 +81,27 @@ export const viewport: Viewport = {
   themeColor: "#FFFFFF",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+// export default function RootLayout({ children }: { children: ReactNode }) {
+//   return (
+//     <html lang="en" dir="ltr">
+//       <head />
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en" dir="ltr">
-      <head />
-      <body>{children}</body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
