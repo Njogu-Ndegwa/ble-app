6b6a0c0eb364b7833f5a50d323ffc18e885417e8
WhitneyMadafali <albrightwhitney93@gmail.com>
2025-05-21 11:51:03 +0300

    Change variables

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,19 +1,19 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-let url = "https://dev-federated-graphql-api.omnivoltaic.com/graphql";
-
-if (process.env.NODE_ENV === "production"){
-  url = "https://federated-graphql-api.omnivoltaic.com/graphql";
-}
+// Determine the API URL based on the current location
+const isWvApp = typeof window !== "undefined" && window.location.origin === "https://wvapp.omnivoltaic.com";
+const apiUrl = isWvApp
+  ? "https://federated-graphql-api.omnivoltaic.com/graphql"
+  : "https://dev-federated-graphql-api.omnivoltaic.com/graphql";
 
 const httpLink = createHttpLink({
-  uri: url,
+  uri: apiUrl,
 });
 
 const authLink = setContext((_, { headers }) => {
-  // Get the authentication token from local storage or wherever it's stored
-  const token = localStorage.getItem("access_token");
+  // Get the authentication token from local storage
+  const token = typeof window !== "undefined" ? localStorage.getItem("access_token") : null;
   return {
     headers: {
       ...headers,
@@ -28,4 +28,34 @@ const apolloClient = new ApolloClient({
 });
 
 export default apolloClient;
+// import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+
+// let url = "https://dev-federated-graphql-api.omnivoltaic.com/graphql";
+
+// if (process.env.NODE_ENV === "production"){
+//   url = "https://federated-graphql-api.omnivoltaic.com/graphql";
+// }
+
+// const httpLink = createHttpLink({
+//   uri: url,
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   // Get the authentication token from local storage or wherever it's stored
+//   const token = localStorage.getItem("access_token");
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
+
+// const apolloClient = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+// export default apolloClient;
 
