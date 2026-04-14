5259c772b57cba5d83faa2898a7f09dd76275dc2
Dennis <dennisndegwa001@gmail.com>
2026-04-14 11:58:28 +0300

    Update Code

--- src/app/loader.tsx
+++ src/app/loader.tsx
@@ -14,6 +14,7 @@ interface ProgressiveLoadingProps {
 
 
 
+
  const ProgressiveLoading: React.FC<ProgressiveLoadingProps> = ({
   initialMessage = "Initializing...",
   completionMessage = "Loading complete!",
